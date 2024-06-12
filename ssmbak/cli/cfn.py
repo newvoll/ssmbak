c378816498808392f739ee35a0df11cc6dd46b0c
oldmouse <olmouse@newvoll.net>
2024-06-12 16:13:14 -0700

    no requirements for lambda, stack del on fail, lambda zipfile hack!

--- ssmbak/cli/cfn.py
+++ ssmbak/cli/cfn.py
@@ -95,6 +95,7 @@ class Stack:
         template_body = dump_yaml(template)
         kwargs = {
             "StackName": self.name,
+            "OnFailure": "DELETE",
             "Parameters": parameters,
             "Capabilities": ["CAPABILITY_NAMED_IAM"],
             "TemplateBody": template_body,
