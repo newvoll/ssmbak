44dba3c3a2c4b8225e4550ade528c3408ab18568
oldmouse <olmouse@newvoll.net>
2024-06-13 16:08:33 -0700

    oops

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../ssmbak/"))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -23,5 +28,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+# added
+extensions = ["sphinx.ext.autodoc"]
